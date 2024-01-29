c38a2a29aee4bf90f29d23ddc2d9e08f8fddadc6
SoyerBC <157618382+SoyerBC@users.noreply.github.com>
2024-01-29 09:52:52 +0100

    Input and saving

--- Assets/Scripts/MainMenuManager.cs
+++ Assets/Scripts/MainMenuManager.cs
@@ -1,23 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MainMenuManager : MonoBehaviour
 {
+    [SerializeField] TextMeshProUGUI PlayerName;
     // Start is called before the first frame update
     void Start()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    
     public void loadGame(int ID)
     {
         SceneManager.LoadScene(ID);
+    }
+
+
+    public void SaveValue(string UsernameInput)
+    {
+        PlayerPrefs.SetString("UserName", UsernameInput); 
+    }
+
+    public void LoadValue()
+    {
+        
+        string playername = PlayerPrefs.GetString("UserName");
+        PlayerName.text = playername;        
     }
 }
